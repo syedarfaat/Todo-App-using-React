ec155d9e38d4affcd50b1de6a6642b2651e2ad4f
syedarfaat <syedarfaat45@gmail.com>
2023-07-05 19:07:10 +0530

    Added Comments

--- src/App.js
+++ src/App.js
@@ -1,55 +1,69 @@
-import { ReactDOM, useReducer, useState } from "react";
-import React from "react";
-import {Button, Container, Form} from 'react-bootstrap'
-import './App.css'
+// Import required modules
+import React, { useReducer, useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import "./App.css";
 import Todos from "./Todos";
 
-export const ACTIONS={
-  ADD_TODO:'add-item',
-  DELETE_TODOS:'delete-todos',
-  DELETE_ONE:'delete-one'
-}
-function reducer(todos,{payload,type})
-  {
-    switch(type)
-    {
-      case ACTIONS.ADD_TODO:
-        return(
-          [...todos,
-           {content: payload.newTodo,
-            id:Date.now()
-          }]
-        )
-        case ACTIONS.DELETE_ONE:
-          return(todos.filter(todo=>(todo.id!==payload.id)))
-        
+// Define actions as constants
+export const ACTIONS = {
+  ADD_TODO: "add-item",
+  DELETE_TODOS: "delete-todos",
+  DELETE_ONE: "delete-one",
+};
 
-        case ACTIONS.DELETE_TODOS:
-          return(todos.filter(todo=>!payload.selectedTodos.includes(todo.id)))
-    }    
-    }
-  
+// Reducer function to manage state updates based on dispatched actions
+function reducer(todos, { payload, type }) {
+  switch (type) {
+    case ACTIONS.ADD_TODO:
+      // Add a new todo item to the existing todos array
+      return [
+        ...todos,
+        { content: payload.newTodo, id: Date.now() }
+      ];
+    case ACTIONS.DELETE_ONE:
+      // Remove a single todo item based on its ID
+      return todos.filter((todo) => todo.id !== payload.id);
+    case ACTIONS.DELETE_TODOS:
+      // Remove multiple todo items based on the list of selected IDs
+      return todos.filter((todo) => !payload.selectedTodos.includes(todo.id));
+    default:
+      return todos; // Return the unchanged todos if the action type is not recognized
+  }
+}
+
 function App() {
-  const [inputValue,setInputValue]=useState('')
-  const [todos,dispatch]=useReducer(reducer,[])
-  
+  // State variables using useState hook
+  const [inputValue, setInputValue] = useState('');
+  const [todos, dispatch] = useReducer(reducer, []);
+
+  // Function to handle form submission
+  const handleSubmit = () => {
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { newTodo: inputValue } });
+  };
+
   return (
-    
     <div className="App">
-    <Container style={{width:'500px' }} className="d-flex justify-content-center mt-5 flex-column">
-      <h1 className="align-self-center text-danger">Todos</h1>
-         <Form>
-         <Form.Group className="align-self-center mt-5">
-         <Form.Control type="text" onChange={(event)=>setInputValue(event.target.value)} value={inputValue} required placeholder="What needs to be done?"/>
-         
-         </Form.Group>
-         <Button onClick={()=>dispatch({type:ACTIONS.ADD_TODO, payload:{newTodo:inputValue}})} className="bg-secondary mt-2">Submit</Button>
-         </Form>
-         <div>
+      <Container style={{ width: '500px' }} className="d-flex justify-content-center mt-5 flex-column">
+        <h1 className="align-self-center text-danger">Todos</h1>
+        <Form>
+          <Form.Group className="align-self-center mt-5">
+            <Form.Control
+              type="text"
+              onChange={(event) => setInputValue(event.target.value)}
+              value={inputValue}
+              required
+              placeholder="What needs to be done?"
+            />
+          </Form.Group>
+          <Button onClick={handleSubmit} className="bg-secondary mt-2">
+            Submit
+          </Button>
+        </Form>
+        <div>
+          {/* Pass the todos and dispatch function to Todos component */}
           <Todos todos={todos} dispatch={dispatch} />
-      </div>
-         </Container>
-      
+        </div>
+      </Container>
     </div>
   );
 }
